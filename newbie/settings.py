8a3c5361f11f6fd7cf49139b75e9d34c7afe71b9
dangkh <dangkh_58@vnu.edu.vn>
2018-11-12 02:55:50 +0700

    add first view, homepage

--- newbie/settings.py
+++ newbie/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store.apps.CatalogConfig',
+    'store.apps.StoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -73,11 +73,14 @@ WSGI_APPLICATION = 'newbie.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'haidang',
+        'PASSWORD': 'haidang',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
@@ -106,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Saigon'
 
 USE_I18N = True
 
--- newbie/urls.py
+++ newbie/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    path('store/', include('store.urls')),
+    path('', RedirectView.as_view(url='/store/', permanent=True)),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
