48c06ae1c2f6048aa24c1ac0a5d9812a6c89e778
dangkh <dangkh_58@vnu.edu.vn>
2018-11-12 05:48:59 +0700

    add login

--- newbie/urls.py
+++ newbie/urls.py
@@ -28,3 +28,7 @@ urlpatterns = [
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 #not sure
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+#adding login logout
