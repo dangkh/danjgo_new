6d350dc99cc5ee0ce6b2bdff14129a9a97feb0e6
dangkh <dangkh_58@vnu.edu.vn>
2018-11-12 03:39:55 +0700

    finish add model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+    city_name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.city_name
+
+
+class Restaurant(models.Model):
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    res_name = models.CharField(max_length=200)
+    res_address = models.CharField(max_length=200)
+    res_description = models.CharField(max_length=400)
+    def __str__(self):
+        return self.Res_name
